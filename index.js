d7cb4cd2f9f1077ef098da4c2c90a2202043bd56
LordCat <noahcatz@outlook.com>
2025-06-06 11:33:23 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express')
+const cors = require('cors')
 const path = require('path')
 const app = express()
 
-app.use(express.static(path.join(__dirname, "public")))
 
-app.get("/api/health", (req, res) => {
-    res.json({health: "OK"})
-})
+app.use(cors({
+    origin: "www.lordcat.dev",
+    methods: ["GET"]
+}))
+app.use(express.static(path.join(__dirname, "public")))
 
 app.listen(3000, () => console.log("Listening on port 3000"))
